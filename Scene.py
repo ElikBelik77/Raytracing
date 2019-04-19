5bbca272081c941459f730ac8cc6956d9de8f0ce
Eliran Ziv <eliran2001@gmail.com>
2019-04-19 06:02:09 +0300

    Fixed radian and angle inconsistency

--- Scene.py
+++ Scene.py
@@ -10,27 +10,29 @@ class Scene(tk.Frame):
         tk.Frame.__init__(self, master)
         self.canvas = tk.Canvas(master, width=1500, height=800, bd=0, highlightthickness=0)
         self.canvas.pack()
-        self.drawing_center = (750,400)
+        self.drawing_center = (750, 400)
         self.camera = Camera(10, 10, Vector(1, 1, 1).normalize(), 30)
         self.cameraGrid = []
 
-    def drawCameraGrid(self):
+    def     drawCameraGrid(self):
         self.canvas.delete("all")
         camera_points = self.camera.getCameraGrid()
-        s = Sphere(90,110,0,30)
+        s = Sphere(90, 110, 0, 30)
         for p in camera_points:
             collisions = s.get_collision(p)
             if s.get_collision(p) is None:
 
                 self.cameraGrid.append(
 
-                    self.canvas.create_line(self.drawing_center[0], self.drawing_center[1], self.drawing_center[0]+p.x, self.drawing_center[1]+p.y))
+                    self.canvas.create_line(self.drawing_center[0], self.drawing_center[1],
+                                            self.drawing_center[0] + p.x, self.drawing_center[1] + p.y))
             else:
                 print("hit")
-                p = Vector(0,0,0).get_closest(collisions)
-                self.canvas.create_oval(self.drawing_center[0]+p.x-2,self.drawing_center[1] + p.y -2 ,self.drawing_center[0]+p.x+2,self.drawing_center[1] + p.y + 2 )
+                p = Vector(0, 0, 0).get_closest(collisions)
+                self.canvas.create_oval(self.drawing_center[0] + p.x - 2, self.drawing_center[1] + p.y - 2,
+                                        self.drawing_center[0] + p.x + 2, self.drawing_center[1] + p.y + 2)
 
-        self.camera.direction = self.camera.direction.add(Vector(0,-0.4,0))
+        self.camera.direction = self.camera.direction.rotate(Vector(0.008, 0, 0))
         self.canvas.after(1, self.drawCameraGrid)  # (time_delay, method_to_execute)
 
 
--- Vector.py
+++ Vector.py
@@ -24,27 +24,26 @@ class Vector:
     def rotate(self, angles):
         return self.rotateX(angles.x).rotateY(angles.y).rotateZ(angles.z)
 
-    def rotateX(self, angle):
+    def rotateX(self, rad):
         """ Rotates the point around the X axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
+
         cosa = math.cos(rad)
         sina = math.sin(rad)
         y = self.y * cosa - self.z * sina
         z = self.y * sina + self.z * cosa
         return Vector(self.x, y, z)
 
-    def rotateY(self, angle):
+    def rotateY(self, rad):
         """ Rotates the point around the Y axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
+
         cosa = math.cos(rad)
         sina = math.sin(rad)
         z = self.z * cosa - self.x * sina
         x = self.z * sina + self.x * cosa
         return Vector(x, self.y, z)
 
-    def rotateZ(self, angle):
+    def rotateZ(self, rad):
         """ Rotates the point around the Z axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
         cosa = math.cos(rad)
         sina = math.sin(rad)
         x = self.x * cosa - self.y * sina
@@ -65,7 +64,7 @@ class Vector:
         return min_point
 
     def get_direction(self):
-        return Vector(math.cos(self.x / self.magnitude()), math.acos(self.y / self.magnitude()),
+        return Vector(math.acos(self.x / self.magnitude()), math.acos(self.y / self.magnitude()),
                       math.acos(self.z / self.magnitude())).normalize()
 
     def multiply(self, v):
